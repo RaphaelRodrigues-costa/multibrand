e51912504eb35c548bacaa1f1433b533e5f76ddc
Raphael Rodrigues <raphael.rodrigues@b4b.ventures>
2023-08-28 03:05:59 -0300

    feat(ValueObject): V.O para ajudar com os dados que vão retornar da req com a web.

---
+++ app/ValueObjects/Car/ListAllCarsVO.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\ValueObjects\Car;
+
+class ListAllCarsVO
+{
+    public ?string $id;
+    public ?string $carName;
+    public ?string $link;
+    public ?string $linkImage;
+    public ?string $year;
+    public ?string $fuel;
+    public ?string $doors;
+    public ?string $kilometers;
+    public ?string $gearbox;
+    public ?string $color;
+    public ?string $price;
+}
